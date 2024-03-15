f2bc8f92d5ac3700cfe0ba550bc10745cc40066c
Tuyet Tran <trinhtuyettran26@gmail.com>
2024-03-15 14:52:38 +0700

    update matkahu

--- Project1/Form1.Designer.cs
+++ Project1/Form1.Designer.cs
@@ -28,43 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtUsername = new System.Windows.Forms.TextBox();
-            this.SuspendLayout();
+            label1 = new Label();
+            txtUsername = new TextBox();
+            label2 = new Label();
+            txtMatkhau = new TextBox();
+            SuspendLayout();
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(80, 44);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(91, 25);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Username";
+            label1.AutoSize = true;
+            label1.Location = new Point(64, 35);
+            label1.Margin = new Padding(2, 0, 2, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(75, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Username";
             // 
             // txtUsername
             // 
-            this.txtUsername.Location = new System.Drawing.Point(279, 49);
-            this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(150, 31);
-            this.txtUsername.TabIndex = 1;
+            txtUsername.Location = new Point(223, 39);
+            txtUsername.Margin = new Padding(2, 2, 2, 2);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(121, 27);
+            txtUsername.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(89, 111);
+            label2.Name = "label2";
+            label2.Size = new Size(70, 20);
+            label2.TabIndex = 2;
+            label2.Text = "Mat khau";
+            // 
+            // txtMatkhau
+            // 
+            txtMatkhau.Location = new Point(278, 126);
+            txtMatkhau.Name = "txtMatkhau";
+            txtMatkhau.Size = new Size(125, 27);
+            txtMatkhau.TabIndex = 3;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.txtUsername);
-            this.Controls.Add(this.label1);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(640, 360);
+            Controls.Add(txtMatkhau);
+            Controls.Add(label2);
+            Controls.Add(txtUsername);
+            Controls.Add(label1);
+            Margin = new Padding(2, 2, 2, 2);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Label label1;
         private TextBox txtUsername;
+        private Label label2;
+        private TextBox txtMatkhau;
     }
 }
