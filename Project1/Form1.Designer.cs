d81616c69ad189760dc71ef66cff5423d74ad75e
Tuyet Tran <trinhtuyettran26@gmail.com>
2024-03-15 15:11:36 +0700

    update login with username pass

--- Project1/Form1.Designer.cs
+++ Project1/Form1.Designer.cs
@@ -28,74 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtUsername = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.txtPassword = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.SuspendLayout();
+            label1 = new Label();
+            txtUsername = new TextBox();
+            button1 = new Button();
+            txtPassword = new TextBox();
+            button2 = new Button();
+            SuspendLayout();
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(80, 44);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(129, 25);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Ten dang nhap";
+            label1.AutoSize = true;
+            label1.Location = new Point(64, 35);
+            label1.Margin = new Padding(2, 0, 2, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(107, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Ten dang nhap";
             // 
             // txtUsername
             // 
-            this.txtUsername.Location = new System.Drawing.Point(279, 49);
-            this.txtUsername.Margin = new System.Windows.Forms.Padding(2);
-            this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(150, 31);
-            this.txtUsername.TabIndex = 1;
+            txtUsername.Location = new Point(223, 39);
+            txtUsername.Margin = new Padding(2);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(121, 27);
+            txtUsername.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(80, 134);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(112, 34);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Mat khau";
-            this.button1.UseVisualStyleBackColor = true;
+            button1.Location = new Point(64, 107);
+            button1.Margin = new Padding(2);
+            button1.Name = "button1";
+            button1.Size = new Size(90, 27);
+            button1.TabIndex = 2;
+            button1.Text = "Mat khau";
+            button1.UseVisualStyleBackColor = true;
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(292, 141);
-            this.txtPassword.Margin = new System.Windows.Forms.Padding(2);
-            this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(150, 31);
-            this.txtPassword.TabIndex = 3;
+            txtPassword.Location = new Point(234, 113);
+            txtPassword.Margin = new Padding(2);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(121, 27);
+            txtPassword.TabIndex = 3;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(245, 202);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(112, 34);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "Dang nhap";
-            this.button2.UseVisualStyleBackColor = true;
+            button2.Location = new Point(196, 162);
+            button2.Margin = new Padding(2, 2, 2, 2);
+            button2.Name = "button2";
+            button2.Size = new Size(90, 27);
+            button2.TabIndex = 4;
+            button2.Text = "Dang nhap";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.txtPassword);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.txtUsername);
-            this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(640, 360);
+            Controls.Add(button2);
+            Controls.Add(txtPassword);
+            Controls.Add(button1);
+            Controls.Add(txtUsername);
+            Controls.Add(label1);
+            Margin = new Padding(2);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
--- Project1/Form1.cs
+++ Project1/Form1.cs
@@ -11,5 +11,24 @@ namespace Project1
         {
 
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            // Ki?m tra thông tin ðãng nh?p
+            if (IsValidLogin(username, password))
+            {
+                // Ðãng nh?p thành công
+                MessageBox.Show("Ðãng nh?p thành công!");
+                // Th?c hi?n các hành ð?ng sau khi ðãng nh?p thành công
+            }
+            else
+            {
+                // Ðãng nh?p th?t b?i
+                MessageBox.Show("Ðãng nh?p không thành công. Vui l?ng ki?m tra l?i tên ðãng nh?p và m?t kh?u.");
+            }
+        }
     }
 }
